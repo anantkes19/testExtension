fdec782859f8ce069ea4ec35f7793f5affc0cc17
anantkes19 <anantkes@colby.edu>
2018-03-16 21:00:17 -0400

    Added login/logout

--- background.js
+++ background.js
@@ -1,11 +1,37 @@
+function sendPageData() {
+  alert("Test Works");
+
+}
+function loginClicked() {
+  $("#save").hide();
+  $("#logout").hide();
+  $("#login").hide();
+
+  $("#username").show();
+  $("#password").show();
+  $("#loginSubmit").show();
+}
+
+function loggedIn() {
+  $("#save").show();
+  $("#logout").show();
+  $("#login").show();
+
+  $("#username").hide();
+  $("#password").hide();
+  $("#loginSubmit").hide();
+}
+
+function onLoad() {
+  $("#save").click(sendPageData);
+
+  loggedIn(); //Call this to hide log in info.
+
+  $("#login").click(loginClicked);
+
+  $("#loginSubmit").click(loggedIn);
+
+}
 
 
-$(function () {
-    $("#cloudSaveButton").click(function () {
-        var name = "5"
-
-       alert("Hi"+" "+name);
-
-
-    });
-});
+$(document).ready(onLoad);

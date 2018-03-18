509b83234f403b95a6dd0750fd2bde0f377e7bca
Austin Nantkes <anantkes@colby.edu>
2018-03-18 13:16:09 -0400

    Added more button functionality

--- background.js
+++ background.js
@@ -1,36 +1,62 @@
 function sendPageData() {
+  //This function will send the save data to the server
   alert("Test Works");
 
 }
-function loginClicked() {
-  $("#save").hide();
-  $("#logout").hide();
-  $("#login").hide();
+function loginToggle() {
+  //This function flips around objects viewing between states
+  $("#login").toggle();
 
-  $("#username").show();
-  $("#password").show();
-  $("#loginSubmit").show();
+  $("#username").toggle();
+  $("#password").toggle();
+  $("#loginSubmit").toggle();
 }
 
-function loggedIn() {
-  $("#save").show();
-  $("#logout").show();
+function init() {
+  //This function ensures things are hidden when they are supposed to be.
+  //if login token exists: Send token to server for validation (with username?)
+  //If yes, show save, logout and hide login
+  //else: hide save, logout and show login
+
+  $("#save").hide();
+  $("#logout").hide();
   $("#login").show();
 
   $("#username").hide();
   $("#password").hide();
   $("#loginSubmit").hide();
 }
+function loginSubmit() {
+  //Send credentials to server
+  //If response is yes, login and call login toggle and userloggedin after. Save a cookie to user with their login token (saved on server too)
+  loginToggle();
+  userStateToggle();
+  //If no, error message explaining
+}
+
+function userStateToggle() {
+  //This function is used for when the user logs in or logs out so proper buttons are shown
+  $("#save").toggle();
+  $("#login").toggle();
+  $("#logout").toggle();
+}
+
+function logout() {
+  //This function logs a user out
+
+  //Delete stored token client side.
+  userStateToggle();
+}
+
 
 function onLoad() {
+  init(); //Call this to check if user logged in
+
+
   $("#save").click(sendPageData);
-
-  loggedIn(); //Call this to hide log in info.
-
-  $("#login").click(loginClicked);
-
-  $("#loginSubmit").click(loggedIn);
-
+  $("#login").click(loginToggle);
+  $("#loginSubmit").click(loginSubmit);
+  $("#logout").click(logout);
 }
 
 

5313796b7387cba6b444fd0f7e91e7593265ba68
Austin Nantkes <anantkes@colby.edu>
2018-04-08 20:35:23 -0400

    Adding save page

--- background.js
+++ background.js
@@ -1,21 +1,58 @@
 function sendPageData() {
   //This function will send the save data to the server
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    console.log(tabs.length);
     var url = tabs[0].url;
+    chrome.storage.sync.get(['ccToken', 'email'], function(result) {
+      var data = {};
+    	data.email = result.email;
+      data.url = url;
+      data.token = result.ccToken;
+      //var returnValue;
+      $.ajax({
+      	type: 'POST',
+      	data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: 'http://24.93.129.131:8080/db/getURL',
+        success: function(data) {
+          console.log(data);
+          if(data.authenticated) {
+            console.log("Article Saved!");
+          } else {
+            //Give error message that email/password was wrong.
+          }
+        }
+      });
+    });
+
     console.log(url + " has been sent to the cloud!");
-});
+  });
 
 }
 
-
-function loginToggle() {
-  //This function flips around objects viewing between states
-  $("#loginInfo").toggle();
-  $("#login").toggle();
-
-
+function hideAll(){
+  $("#login").hide();
+  $("#loginInfo").hide();
+  $("#save").hide();
+  $("#login").hide();
+  $("#logout").hide();
 }
 
+function logInPage() {
+  hideAll();
+  $("#loginInfo").show();
+}
+
+function homePage() {
+  hideAll();
+  $("#login").show();
+}
+
+function loggedInPage() {
+  hideAll();
+  $("#logout").show();
+  $("#save").show();
+}
 
 function init() {
   //This function ensures things are hidden when they are supposed to be.
@@ -24,13 +61,17 @@ function init() {
   //else: hide save, logout and show login
   chrome.storage.sync.get(['ccToken', 'email'], function(result) {
     console.log(result);
-    sendLoginData(result.email, "null", result.ccToken, true);
-
+    loggedIn = sendLoginData(result.email, "null", result.ccToken);
+    if(loggedIn) {
+      loggedInPage();
+    } else {
+      homePage();
+    }
   });
 
 }
 
-function sendLoginData(email, password, token, initPhase) {
+function sendLoginData(email, password, token) {
   var data = {};
 	data.email = email;
   userEmail = email;
@@ -45,20 +86,16 @@ function sendLoginData(email, password, token, initPhase) {
   success: function(data) {
     console.log(data);
     if(data.authenticated) {
-      if(initPhase) {
-        $("#login").hide();
-        $("#loginInfo").hide();
-        return;
-      }
 
       chrome.storage.sync.set({'ccToken': data.token, 'email':userEmail}, function() {
               console.log('ccToken is set to ' + data.token);
               console.log('email set to ' + userEmail);
-              userStateToggle();
-              loginToggle();
+              loggedInPage();
+              return data.authenticated;
         });
     } else {
       //Give error message that email/password was wrong.
+      return false;
     }
   }
 });
@@ -69,16 +106,8 @@ function loginSubmit() {
 	email = $("#username").val();
   password = $("#password").val();
   token = "0000";
-  sendLoginData(email, passord, token);
+  sendLoginData(email, password, token);
 }
-
-function userStateToggle() {
-  //This function is used for when the user logs in or logs out so proper buttons are shown
-  $("#save").toggle();
-  $("#login").toggle();
-  $("#logout").toggle();
-}
-
 
 function logout() {
   //This function logs a user out
@@ -93,7 +122,7 @@ function logout() {
     });
   })
   //Delete stored token client side.
-  userStateToggle();
+  homePage();
 }
 
 
@@ -102,7 +131,7 @@ function onLoad() {
 
 
   $("#save").click(sendPageData);
-  $("#login").click(loginToggle);
+  $("#login").click(logInPage);
   $("#loginSubmit").click(function( event ) {
     event.preventDefault();
     loginSubmit();
